c94087ac15d9149ea9b22a1a9915de8e83aa9d76
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-08-15 05:58:26 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Lisa"
+  end
+
+  get '/hometown' do
+    "My hometown is Belleville"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Adelina"
+  end
 end
